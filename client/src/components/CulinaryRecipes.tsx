e3470fe24463df898e64759051699285f30b5a31
Paulina <paulinaluksa1@gamil.com>
2023-12-28 22:08:02 +0100

    Poprawka RecipeForm; dodanie opcji wyboru filtrow

--- client/src/components/CulinaryRecipes.tsx
+++ client/src/components/CulinaryRecipes.tsx
@@ -1,8 +1,8 @@
+import type {CSSProperties} from 'react';
 import React, {useState} from 'react';
 import {CaretRightOutlined, ClockCircleOutlined} from '@ant-design/icons';
-import type {CSSProperties} from 'react';
-import {Collapse, Input, theme, Tooltip} from 'antd';
 import type {CollapseProps} from 'antd';
+import {Collapse, theme, Tooltip} from 'antd';
 import {ActionIcon, Rating} from "@mantine/core";
 import {SearchCulinaryRecipes} from "../features/Recipe/SearchCulinaryRecipes";
 import {IconTablePlus} from "@tabler/icons-react";
@@ -22,7 +22,7 @@ const getItems: (panelStyle: CSSProperties,
                 <p>{text}</p>
                 <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'flex-end'}}>
                     <Rating value={values[0]} onChange={(value) => setValues([value, values[1], values[2]])}/>
-                    <Tooltip title="Czas przygotowania">
+                    <Tooltip title="Preparation time">
                         <ClockCircleOutlined style={{marginRight: '10px'}}/>
                         <span>5 min</span>
                     </Tooltip>
@@ -39,7 +39,7 @@ const getItems: (panelStyle: CSSProperties,
                 <p>{text}</p>
                 <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'flex-end'}}>
                     <Rating value={values[1]} onChange={(value) => setValues([values[0], value, values[2]])}/>
-                    <Tooltip title="Czas przygotowania">
+                    <Tooltip title="Preparation time">
                         <ClockCircleOutlined style={{marginRight: '8px'}}/>
                         <span>50 min</span>
                     </Tooltip>
@@ -56,7 +56,7 @@ const getItems: (panelStyle: CSSProperties,
                 <p>{text}</p>
                 <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'flex-end'}}>
                     <Rating value={values[2]} onChange={(value) => setValues([values[0], values[1], value])}/>
-                    <Tooltip title="Czas przygotowania">
+                    <Tooltip title="Preparation time">
                         <ClockCircleOutlined style={{marginRight: '8px'}}/>
                         <span>20 min</span>
                     </Tooltip>
@@ -80,15 +80,20 @@ export const CulinaryRecipes = () => {
     };
 
     const navigate = useNavigate();
-
     return (
         <div className="content">
-            <SearchCulinaryRecipes/>
-            <ActionIcon onClick={() => navigate('/new')} className="icon" variant="outline" color="gray" size="lg"
-                        radius="md"
-                        aria-label="Settings">
-                <IconTablePlus style={{width: '70%', height: '70%'}} stroke={1.5}/>
-            </ActionIcon>
+            <div className="search">
+                <div style={{display: 'flex', flexDirection: 'column'}}>
+                    <SearchCulinaryRecipes/>
+                </div>
+                <ActionIcon onClick={() => navigate('/new')} className="icon" variant="filled" color="#027926"
+                            size="lg"
+                            radius="md"
+                            aria-label="Settings">
+                    <IconTablePlus style={{width: '70%', height: '70%'}} stroke={1.5}/>
+                </ActionIcon>
+            </div>
+
             <Collapse
                 bordered={false}
                 expandIcon={({isActive}) => <CaretRightOutlined rotate={isActive ? 90 : 0}/>}
