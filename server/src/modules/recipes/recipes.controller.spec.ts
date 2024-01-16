e84be1ef8eddbd7802f60d55c2ca0e155779db3e
Jakub Nowak <njakub002@gmail.com>
2024-01-16 21:17:27 +0100

    Backend - testy jednostkowe dla kilku funkcji z modulu recipes

--- server/src/modules/recipes/recipes.controller.spec.ts
+++ server/src/modules/recipes/recipes.controller.spec.ts
@@ -1,18 +1,73 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RecipesController } from './recipes.controller';
+import { RecipesService } from './recipes.service';
+import { TokenGuard } from '../auth/guards/token.guard';
+import { FilterRecipesDto } from './dto/filter-recipes.dto';
+import { RecipeNotfoundException } from '../../exceptions/recipe-notfound-exception';
 
 describe('RecipesController', () => {
   let controller: RecipesController;
+  const mockRecipesService = {
+    findAllRecipes: jest.fn(() => {
+      return [];
+    }),
+    filterRecipes: jest.fn(() => {
+      return [];
+    }),
+    getById: jest.fn((id: number) => {
+      if (id >= 0) return {};
+      else return null;
+    }),
+  };
+  const mockTokenGuard = {};
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [RecipesController],
-    }).compile();
+      providers: [RecipesService],
+    })
+      .overrideProvider(RecipesService)
+      .useValue(mockRecipesService)
+      .overrideGuard(TokenGuard)
+      .useValue(mockTokenGuard)
+      .compile();
 
     controller = module.get<RecipesController>(RecipesController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('getRecipes', () => {
+    it('should return all recipes when no filter is provided', async () => {
+      const result = await controller.getRecipes({});
+      expect(result).toEqual([]);
+      expect(mockRecipesService.findAllRecipes).toHaveBeenCalled();
+    });
+
+    it('should return filtered recipes when filter is provided', async () => {
+      const filter: FilterRecipesDto = {
+        search: 'test',
+        sortOrder: 'asc',
+        sortBy: 'calories',
+      };
+      const result = await controller.getRecipes(filter);
+      expect(result).toEqual([]);
+      expect(mockRecipesService.filterRecipes).toHaveBeenCalledWith(filter);
+    });
+  });
+
+  describe('getRecipe', () => {
+    it('should return a recipe by id', async () => {
+      const recipeId = 1;
+      const result = await controller.getRecipe(recipeId);
+      expect(result).toEqual({});
+      expect(mockRecipesService.getById).toHaveBeenCalledWith(recipeId);
+    });
+
+    it('should throw RecipeNotFoundException for non-existent recipe', async () => {
+      const invalidRecipeId: number = -90;
+      await expect(controller.getRecipe(invalidRecipeId)).rejects.toThrow(
+        RecipeNotfoundException,
+      );
+      expect(mockRecipesService.getById).toHaveBeenCalledWith(invalidRecipeId);
+    });
   });
 });
--- server/src/modules/recipes/recipes.service.spec.ts
+++ server/src/modules/recipes/recipes.service.spec.ts
@@ -1,18 +1,65 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RecipesService } from './recipes.service';
+import { PrismaClient } from '@prisma/client';
+import { mockDeep, DeepMockProxy } from 'jest-mock-extended';
+import { PrismaService } from '../prisma/prisma.service';
 
 describe('RecipesService', () => {
   let service: RecipesService;
+  let prisma: DeepMockProxy<PrismaClient>;
+
+  const mockRecipes = [
+    {
+      id: 0,
+      name: 'test1',
+      rating: 0,
+      reviews: 0,
+      description: 'tester tester',
+      date: Date.now(),
+      totalTime: 0,
+      calories: 0,
+      fat: 0,
+      carbs: 0,
+      protein: 0,
+    },
+    {
+      id: 1,
+      name: 'test1',
+      rating: 1,
+      reviews: 1,
+      description: 'tester tester',
+      date: Date.now(),
+      totalTime: 1,
+      calories: 1,
+      fat: 1,
+      carbs: 1,
+      protein: 1,
+    },
+  ];
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [RecipesService],
-    }).compile();
+      providers: [RecipesService, PrismaService],
+    })
+      .overrideProvider(PrismaService)
+      .useValue(mockDeep<PrismaClient>())
+      .compile();
 
     service = module.get<RecipesService>(RecipesService);
+    prisma = module.get(PrismaService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  // describe('findAllRecipes', () => {
+  //   it('should return an array of recipes', async () => {
+  //     prisma.user.findMany.mockResolvedValueOnce(mockRecipes);
+  //
+  //     const result = await service.findAllRecipes();
+  //     expect(result).toEqual(mockRecipes);
+  //     expect(prisma.recipes.findMany).toHaveBeenCalled();
+  //   });
+  // });
 });
